f27cc97e0b7ace48f5df77e70598c7a4e6da885d
HongHaeIn <w2020@e-mirim.hs.kr>
2021-08-27 10:17:48 +0900

    package

--- module, package/do.py
+++ module, package/do.py
@@ -3,3 +3,29 @@ import my_module as mm
 mm.해인이1()
 mm.해인이2()
 print(mm.date)
+
+import travel.gangreung as ga
+gp = ga.GangreungPackage()
+print(gp)
+
+from travel import gangreung
+gp = gangreung.GangreungPackage()
+print(gp)
+
+from travel.gangreung import GangreungPackage
+gp = GangreungPackage()
+
+
+from travel import ilsan
+ilsan.이지()
+
+import travel.ilsan
+travel.ilsan.이지()
+
+from travel.ilsan import 이지
+이지()
+
+from travel import *
+ilsan.이지()
+gp = gangreung.GangreungPackage()
+print(gp)
