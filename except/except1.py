a6e20a0cb07bec706766a8da0dfa1d3883f1b027
hae in hong <w2020@e-mirim.hs.kr>
2021-09-14 12:01:30 +0900

    예외

---
+++ except/except1.py
@@ -0,0 +1,28 @@
+'''
+java
+try{
+    //예외가 의심되는 코드
+}catch(FileNotFoundException e){
+    //예외가 났을 때 처리하는 코드
+    FileNotFoundException 처리하는 코드
+}finally{
+    //무조건 실행해야하는 코드
+'''
+# print(int('안녕')) #ValueError: invalid literal for int() with base 10: '안녕'
+# list1 = [1, 2, 3]
+# print(list1[0])
+# print(list1[1])
+# print(list1[2])
+#print(list1[3]) #IndexError: list index out of range
+
+
+#print(list2) #NameError: name 'list2' is not defined
+print(10/10)
+print(5/10)
+print(1/10)
+print(0/10)
+print(10/5)
+print(10/1)
+#print(10/0) #ZeroDivisionError: division by zero
+
+#print('나이는'+18) #TypeError: can only concatenate str (not "int") to str
