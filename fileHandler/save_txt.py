909b46440d4bfbce1e8572a576d8e41e47b932a4
hae in hong <w2020@e-mirim.hs.kr>
2021-10-03 22:44:19 +0900

    1

--- fileHandler/save_txt.py
+++ fileHandler/save_txt.py
@@ -1,8 +1,14 @@
 
-f = open('text.txt', 'w', encoding='UTF-8')
+# f = open('text.txt', 'w', encoding='UTF-8')
+#
+# f.write('hello')
+# f.write('\n')
+# f.write('월드')
+#
+# f.close()
 
-f.write('hello')
-f.write('\n')
-f.write('월드')
-
-f.close()
+i = 1
+if(i ==1):
+    print(False)
+else:
+    print(True)
